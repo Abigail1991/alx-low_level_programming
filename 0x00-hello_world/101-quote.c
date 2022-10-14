8114bbf89bcc4106d3b45cbd72b6088c7a923fc5
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-13 18:36:49 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,22 @@
-#include <stdio.h>
+/*
+ *Quote : 101
+ *Author: Brennan
+ */
+
 #include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a new line.
+ * 
+ * Return: Always 1
+ * */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	Return(1);
+	write(2,
+		"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	       	58);
+
+	Return(1)
+
 }
