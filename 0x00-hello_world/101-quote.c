d10c9b5b6b8cdc532032a7632f586b8e777b4c4f
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-13 17:55:52 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	Return(1);
 }
