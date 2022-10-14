8335484dccbf2cf47d09ac35d2663cf29f7016d5
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-13 18:17:32 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
- * main - Prints
- * Return: Always 1
- */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
