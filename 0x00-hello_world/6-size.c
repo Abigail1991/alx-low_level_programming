02a91c3237072f59a14c2207619bf37eb65e150f
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-13 18:52:48 -0700

    size git push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte", sizeof(char));
+	printf("Size of a char: %lu byte\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
