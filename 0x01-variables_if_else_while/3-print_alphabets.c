c001a7f71bcc6f8e51c2d2512018362e1f556602
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-14 18:59:20 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	char e;
+
+	c = 'a';
+	e = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(e <= 'z') {
+			putchar(c);
+			e++;
+		}
+	return (0);
+}
