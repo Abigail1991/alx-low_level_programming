b0781231a77cf27e3cd9405a4e8ee7c1e06dacdf
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-14 20:38:37 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,18 +5,17 @@
  * Return: Always 0
  */
 int main(void)
-	{
+{
 	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = 48; a < 58; a++)
 	{
-		putchar(a + '0');
-		if (a < 9)
-		}
+		putchar(a);
+		if (a != 57)
+		{
 			putchar(',');
 			putchar(' ');
 		}
-	}
 	putchar('\n');
 	return (0);
-	}
+{
