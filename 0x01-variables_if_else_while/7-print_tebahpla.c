14b60f9b2714e10db232d357a922eaff36d2e835
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-14 19:49:20 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char v;
+
+	v = 'z';
+	while
+		(v >= 'a') {
+			putchar(v);
+			v++;
+		}
+	putchar('\n');
+	return (0);
+}
