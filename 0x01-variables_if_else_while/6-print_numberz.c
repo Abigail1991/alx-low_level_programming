1e33e547266c6624158abfee6986ef4ed091d800
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-14 19:42:41 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int v;
+
+	v = 0;
+	while
+		(v < 10) {
+			putchar(v + '0');
+			v++;
+		}
+	putchar('\n');
+	return (0);
+}
