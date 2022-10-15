8b13690cd5d9e0bbc2fbb08e5ce34abf846dcd03
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-14 20:01:23 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char f;
+
+	int j;
+
+	f = 'a';
+	j = 0;
+	while
+		(j < 10) {
+			putchar(j + '0');
+			j++;
+		}
+	while
+		(f <= 'f') {
+			putchar(f);
+			f++
+		}
+	putchar('\n');
+	return (0);
+}
