b891d55044bf4265a80b4375e52b2a6d9bc0d1f2
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-18 13:31:18 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet in lower cases
+ */
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+	_putchar('\n');
+}
