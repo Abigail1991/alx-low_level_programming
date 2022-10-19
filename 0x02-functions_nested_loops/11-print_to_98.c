5117c0171e7772af5e476eb92fadd0304b00adf7
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-18 18:06:48 -0700

    n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#inclide "main.h"
+
+/**
+ * print_to_98 - print all n atural numbers n to 98
+ * @n: the starting number
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d. ", n++);
+		printf("%d\n", n);
+
+	}
+
+}
