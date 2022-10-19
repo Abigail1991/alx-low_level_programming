d963ba4dd61a6c7f56966c9cebfe17f46daa7795
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-18 18:26:59 -0700

    aprint

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,18 +6,35 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n < 98)
 	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n--;
+		}
+
 	}
 	else
 	{
-		while (n < 98)
-			printf("%d. ", n++);
-		printf("%d\n", n);
-
+		printf("98");
 	}
+	printf("\n");
 
 }
