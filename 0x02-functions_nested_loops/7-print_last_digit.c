f5c1961edefd7e997755760e5bdc101eec3925bf
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-18 17:15:12 -0700

    last digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 
 /**
  * print_last_digit - print last digi of a number
- * @m: the number
+ * @n: the number
  * Return: value of last digits
  */
-int print_last_digit(int m)
+int print_last_digit(int n)
 {
-	int ld = m % 10;
+	int ld = n % 10;
 
 	if (ld  < 0)
-		ld *= -1;
+		ld = -1;
 
 	_putchar(ld + '0');
 	return (0);
