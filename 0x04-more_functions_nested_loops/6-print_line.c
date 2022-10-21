2ae4558c6ee82d88c56c7eabaab336a04cc1ec52
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-21 15:15:01 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,15 +3,23 @@
 /**
  * print_line - Draws a straight line using the character_
  * @n: The number of _ characters to be printed
+ * Return: Straight line
  */
 void print_line(int n)
 {
-	int lin:
+	int con;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (lin = 0; lin < n; len++)
-			_putchar('_');
+		_putchar('\n');
 	}
+	else
+	{
+		for (con = 1; con <= n; con++)
+	{
+			_putchar('_');
+		}
 	_putchar('\n');
+
+	}
 }
