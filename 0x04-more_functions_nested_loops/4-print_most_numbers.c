9dc4a5bdffea642ced1c54a4b8dfe5320e16abe6
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-20 19:29:17 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	while (c < 0)
 	{
-		if (!(c == 2 || c == 4))
-			_putchar(c);
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+		c++;
 	}
-	_Putchar('\n')
+	_Putchar('\n');
 }
 
