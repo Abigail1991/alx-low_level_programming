cc4e33c9f63793703b2a74b2406858eb369cc258
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-21 12:32:41 -0700

    draw line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character_
+ * @n: The number of _ characters to be printed
+ */
+void print_line(int n)
+{
+	int lin:
+
+	if (n > 0)
+	{
+		for (lin = 0; lin < n; len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
