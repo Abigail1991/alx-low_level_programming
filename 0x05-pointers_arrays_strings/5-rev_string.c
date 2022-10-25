dfa650fa96664c4717a826ba2a4cda97d1932d40
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-24 18:38:51 -0700

    reverse print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverse a string
+ * @s: string
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int j;
+	int c;
+	char rev = s[0];
+
+	while (s[c] != '\0')
+		c++;
+	for (j = 0; j < c; j++)
+	{
+		c--;
+		rv = s[j];
+		s[j] = s[c];
+		s[c] = rv;
+	}
+}
