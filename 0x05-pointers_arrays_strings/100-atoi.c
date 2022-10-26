ab9d0106c184c64e3f9b9f419687154ed47d9ca8
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-25 21:19:28 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,13 +13,12 @@ int _atoi(char *s)
 	unsigned int num = 0;
 
 	{
-	if (*s == ' -')
+	if (*s == '-')
 		x *= -1;
 	else if (*s >= '0' && *s <= '9')
 		num = (num * 10) + (*s - '0');
 	else if (num > 0)
 		break;
-	}
-	while (*s++);
+	}	while (*s++);
 	return (num * x);
 }
