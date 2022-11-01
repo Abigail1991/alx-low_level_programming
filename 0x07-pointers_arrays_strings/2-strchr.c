0d377a777186c8ed87e5e6571dcc604fdf8ddf96
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-31 18:52:12 -0700

    string to char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,19 +4,16 @@
  * _strchr - a funtion that locates character in a string
  * @c: character
  * @s: string
- * Return: NULL
+ * Return: NULL if c is not found
  */
 char *_strchr(char *s, char c)
 {
-	int a = 0, b;
+	int a;
 
-	while (s[a])
-		a++;
-	for (b = 0; b < a; b++)
+	for (a = 0; s[a] >= '\0'; a++)
 	{
-		if (c == s[b])
-		s += b;
-		return (s);
+		if (c == s[a])
+		return (s + a);
 	}
 	return ('\0');
 }
