b051489304011b493a3e87cce1b4f0374520f5d7
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-10-31 19:07:09 -0700

    string substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,12 +15,12 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (a = 0; accept[a]; a++)
 		{
-			if (*s == accept[index])
+			if (*s == accept[a])
 			{
 				bytes++;
 				break;
 			}
-			else if (accept[index + 1] == '\0')
+			else if (accept[a + 1] == '\0')
 				return (bytes);
 		}
 		s++
