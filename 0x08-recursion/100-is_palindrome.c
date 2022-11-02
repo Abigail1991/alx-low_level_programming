e79f6973cc083cefdc2911991edb82029b6981ab
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-02 16:43:31 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,34 +15,37 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * pal - palindrome
+ * p1 - palindrome
  * @s: pointer
  * @j: position
  * Return: boolen
  */
-int pal(char *s, int j)
+int p1(char *s, int j)
 {
+/*lets use boolen to solve*/
+
 	if (j < 1)
 	{
-	return (1);
+		return (1);
 	}
 
 	if (*s == *(s + j))
 	{
-	return (pal(s + 1, j - 2));
-}
+		return (p1(s + 1, j - 2));
+	}
+	return (0);
 }
 
 /**
  * is_palindrome - palindrom function
  * @s: pointer
- * Return: 1 if pal and 0 if not pal
+ * Return: recursion
  */
 int is_palindrome(char *s)
 {
+/* now lets palindrome */
+
 	int l = _strlen_recursion(s);
 
-	{
-	return (pal(s, l - 1));
-	}
+	return (p1(s, l - 1));
 }
