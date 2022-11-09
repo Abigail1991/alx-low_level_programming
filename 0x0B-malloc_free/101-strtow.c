46513d9707aae8df35556fcd74ccc5236432d05a
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-09 12:58:02 -0800

    push

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -70,7 +70,7 @@ char **strtow(char *str)
 		while (str[index] == ' ')
 			index++;
 		letters = word_len(str + index);
-		strings[w] = malloc(sizeof(char) * (letters + 1));
+		strings*[w] = malloc(sizeof(char) * (letters + 1));
 
 		if (strings[w] == NULL)
 		{
