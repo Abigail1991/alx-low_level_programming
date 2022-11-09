0516d8781069b886e6dac8614b1d08f1d501b1cf
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-09 12:49:01 -0800

    2 strings

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -69,7 +69,7 @@ char **strtow(char *str)
 		while (str[index] == ' ')
 			index++;
 		letters = word_len(str + index);
-		strings[w] = malloc(sizeof(char) * (letters + 1));
+		**strings[w] = malloc(sizeof(char) * (letters + 1));
 
 		if (strings[w] == NULL)
 		{
