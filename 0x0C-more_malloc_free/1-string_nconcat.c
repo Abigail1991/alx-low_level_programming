d451c8b6c4735e27272101e6d8a89b1f77c9cc24
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-10 19:03:24 -0800

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,6 +24,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		size2++;
 	}
+	if ( n > size2)
+		n = size2;
+	p = malloc ((size1 + n + 1) * sizeof(char));
+
 	if (p == NULL)
 		return (0);
 	for (a = 0; a < size1; a++)
