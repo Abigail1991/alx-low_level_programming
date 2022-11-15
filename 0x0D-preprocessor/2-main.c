dfd74670fb9e8e86f0b4dd6b527792799f909acf
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-14 17:25:56 -0800

    function like macr

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of file compiled from
+ * Return: Always 0
+ */
+int main(void)
+{
+	char file;
+
+	printf("%s\n", file);
+	return (0);
+}
