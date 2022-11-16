1a2a21863c3011986de12c615b776278a63ca4bb
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-16 13:39:36 -0800

    iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a;
 
-	if (array == NULL && action == NULL)
+	if (array == NULL )
+		return;
+	if (action == NULL)
 		return;
 	for (a = 0; a < size; a++)
 		action(array[a]);
