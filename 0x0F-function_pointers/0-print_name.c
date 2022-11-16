cf0dac4bde5d41e5e8197ad92d5d2bf84b5a4d49
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-16 12:44:42 -0800

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - a function that prints name
+ * @name: name of the person
+ * Return: Nothing
+ * @f: funtion
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	return;
+	f(name);
+}
